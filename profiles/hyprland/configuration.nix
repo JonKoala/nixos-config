4ae0592b449a8191669758700fe27cf9fe4fa469
JonKoala <victor.santana.lemos@gmail.com>
2024-10-16 00:16:43 -0300

    add configurations to turn on numlock during bootup and hyprland startup

--- profiles/hyprland/configuration.nix
+++ profiles/hyprland/configuration.nix
@@ -11,13 +11,18 @@
     enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
-  programs.thunar.enable = true;
 
+  programs.thunar.enable = true;
   services.udisks2.enable = true;
   services.greetd = {
     enable = true;
     settings.default_session.command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --asterisks --time --time-format '%d/%m/%Y %H:%M:%S' --cmd hyprland";
-  };
+  }; 
+
+  # activates numlock on bootup
+  boot.initrd.preLVMCommands = ''
+    ${pkgs.kbd}/bin/setleds +num
+  '';
 
 }
 
