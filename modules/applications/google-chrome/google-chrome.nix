50606f575c419b20d1ac06d841a9202ef13a86f1
JonKoala <victor.santana.lemos@gmail.com>
2025-05-25 20:05:08 -0300

    remove all arguments used when calling chrome
    they were causing issues when rendering some pages and playing videos, and I think the gpu acceleration wasnt really working as intended

--- modules/applications/google-chrome/google-chrome.nix
+++ modules/applications/google-chrome/google-chrome.nix
@@ -5,5 +5,5 @@ let
 in {
   home.packages = [ pkgs.google-chrome ];
   home.file.".local/share/applications/google-chrome.desktop".source = config.lib.file.mkOutOfStoreSymlink "${currentDir}/google-chrome.desktop";
-  programs.bash.shellAliases.chrome = "google-chrome-stable --use-angle=vulkan --use-cmd-decoder=passthrough --ozone-platform=wayland";
+  programs.bash.shellAliases.chrome = "google-chrome-stable";
 }
