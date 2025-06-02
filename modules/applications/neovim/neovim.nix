ac31c6858fc21147ffbbbc6aa1db9688d7b256fa
JonKoala <victor.santana.lemos@gmail.com>
2025-06-02 12:26:22 -0300

    enable and configure lsp for neovim; add lua language and lsp packages

--- modules/applications/neovim/neovim.nix
+++ modules/applications/neovim/neovim.nix
@@ -3,6 +3,10 @@
 let
   currentDir = "${import ../pwd.nix}/neovim";
 in {
+  home.packages = [
+    pkgs.lua
+    pkgs.lua-language-server
+  ];
   programs.neovim = {
     enable = true;
 
@@ -11,8 +15,10 @@ in {
       pkgs.vimPlugins.gruvbox-nvim
       pkgs.vimPlugins.yazi-nvim
       pkgs.vimPlugins.mini-icons
+      pkgs.vimPlugins.lazydev-nvim
     ];
   };
   xdg.configFile."nvim/init.lua".source = config.lib.file.mkOutOfStoreSymlink "${currentDir}/init.lua";
+  xdg.configFile."nvim/lsp/".source = config.lib.file.mkOutOfStoreSymlink "${currentDir}/lsp/";
 }
 
