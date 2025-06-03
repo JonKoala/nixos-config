0ce891073529f69878705908758b15ca34abe545
JonKoala <victor.santana.lemos@gmail.com>
2025-06-02 23:35:58 -0300

    disable lua_ls semantic highlight; add treesitter grammars package; suppress nonsensical warning for lazyvim config

--- modules/applications/neovim/init.lua
+++ modules/applications/neovim/init.lua
@@ -22,7 +22,10 @@ vim.keymap.set("n", "<Leader>fb", function() snacks.picker.buffers(snacks_picker
 
 vim.api.nvim_create_user_command("Yazi", function() require("yazi").yazi() end, { desc = "Open yazi at the current file" })
 
+
 vim.lsp.enable("lua_ls")
+
+---@diagnostic disable-next-line: missing-fields
 require("lazydev").setup({
   ft = "lua"
 })
--- modules/applications/neovim/neovim.nix
+++ modules/applications/neovim/neovim.nix
@@ -16,6 +16,7 @@ in {
       pkgs.vimPlugins.yazi-nvim
       pkgs.vimPlugins.mini-icons
       pkgs.vimPlugins.lazydev-nvim
+      pkgs.vimPlugins.nvim-treesitter.withAllGrammars
     ];
   };
   xdg.configFile."nvim/init.lua".source = config.lib.file.mkOutOfStoreSymlink "${currentDir}/init.lua";
