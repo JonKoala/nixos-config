4d1e41b0cbdd6ad1e0fd0e6076858895ecddb5c5
JonKoala <victor.santana.lemos@gmail.com>
2025-07-27 22:49:35 -0300

    add godot4.4-mono package; change godot desktop files; change godot alias

--- modules/applications/godot/godot.nix
+++ modules/applications/godot/godot.nix
@@ -3,7 +3,12 @@
 let
   currentDir = "${import ../pwd.nix}/godot";
 in {
-  home.packages = [ pkgs.godot_4 ];
+  home.packages = [
+    pkgs.godot_4_4
+    pkgs.godot_4_4-mono
+  ];
   home.file.".local/share/applications/org.godotengine.Godot4.desktop".source = config.lib.file.mkOutOfStoreSymlink "${currentDir}/godot.desktop";
-  programs.bash.shellAliases.godot = "godot4 --display-driver wayland";
+  home.file.".local/share/applications/org.godotengine.Godot4.4-mono.desktop".source = config.lib.file.mkOutOfStoreSymlink "${currentDir}/godot4.4-mono.desktop";
+  home.file.".local/share/applications/org.godotengine.Godot4.4.desktop".source = config.lib.file.mkOutOfStoreSymlink "${currentDir}/godot4.4.desktop";
+  programs.bash.shellAliases.godot = "godot4.4-mono --display-driver wayland";
 }
